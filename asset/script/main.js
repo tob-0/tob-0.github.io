01563a143adf6cd8956f4ec34e7d6992331c4f0f
tob-0 <tbsx@outlook.fr>
2021-06-08 16:58:10 +0200

    [refacto] other

--- asset/script/main.js
+++ asset/script/main.js
@@ -1,15 +1,11 @@
 const loadContent = ()=>{
-    console.debug('Content loading...');
     var contentId = location.hash;
-    console.debug(`contentId=${contentId}`)
     var html_file = `/asset/src/${contentId.substr(1)}.html`
-    console.debug(`html_file=${html_file}`);
     $(()=>{$("#contentLocation").load(html_file)})
     window.scrollTo(0,0)
 }
 if (!location.hash) {
     location.hash="#presentation"
 }
-
 loadContent()
 window.addEventListener("hashchange",loadContent)
