f3d2dc3640561c5742c0bc6d60b2b48f0966f7c7
tob-0 <tbsx@outlook.fr>
2021-06-08 16:57:40 +0200

    [refacto] change display of projects

--- asset/script/projects.js
+++ asset/script/projects.js
@@ -1,51 +1,19 @@
-function formatToId(text) {
-    return `projects-${text.toLowerCase().replaceAll(' ','-')}`
-}
+$.getJSON('/asset/data/projects-tree.json', (projectTypes)=>{
+    finalStr = "projects/\n"
+    for (const key in projectTypes) {
+        objKey = Object.keys(projectTypes)
+        idxKey = objKey.indexOf(key)
+        nbProjects = key.length
+        symbol = (idxKey == objKey.length -1) ? "└── " : "├── "
+        finalStr += symbol + key + "/\n"
 
-projectTypes = document.getElementsByClassName('project-type')
-for (const type of projectTypes) {
-    type.addEventListener('click', () => {
-        id = formatToId(type.innerText)
-        
-        display = document.getElementById(id).style.display
-        if (display != 'block') {
-            document.getElementById(id).style.display='block'
-            type.style.backgroundColor="violet"
+        for (const project of projectTypes[key]) {
+            idxProject = projectTypes[key].indexOf(project)
+            symbol = (idxProject ==projectTypes[key].length -1) ? "└── " : "├── "
+            padding = (nbProjects && idxKey != objKey.length -1) ? "│   ":"    "
+            finalStr+= `${padding}${symbol}<a href=${project.link} target=_blank>${project.title}</a>/\n`
         }
-        else{
-            document.getElementById(id).style.display ='none'
-            type.style.backgroundColor="black"
-        }
-    })
-        } 
-
-
-
-
-let projectsCardHTML={"projects-ada-tech-school":"","projects-perso":""};
-$.getJSON('/asset/data/projects.json', (projects)=>{
-    for (const project of projects) {
-        projectsCardHTML[project.label]+=`
-<div class="project-card size-l">
-    <div class="card-elements">
-        <div class="card-text">
-        <p class="card-title">${project.title}</p>
-        <p class="card-shortdesc">${project.shortdesc}</p>
-        <div class="card-details">
-            <span class="project-languages">${project.languages.join(', ')}</span>
-        </div>
-        </div>
-    </div>
-    <div class="card-elements">
-        <div class="card-image">
-        <a href="${project.link}" target='_blank'><img src="${project.image_link}" alt="${project.image_alt}"></a>
-        </div>
-    </div>
-</div>`
     }
-    for (const projType in projectsCardHTML) {
-        document.getElementById(projType).innerHTML=`
-        <div class="container">${projectsCardHTML[projType]}
-        </div>`
-    }
+    document.getElementById("projects").innerHTML="<pre>"+finalStr+"</pre>"
+    console.log(finalStr)
 })
